89bf9b57f13120de50414a1d8559964e0bc2f522
Calvin Chau <cachau33@gmail.com>
2023-02-21 11:48:08 -0800

    Removed skip from test and completed test

--- src/book.js
+++ src/book.js
@@ -1,12 +1,38 @@
 function createTitle() {
-  
+
 }
 
 module.exports = {
-  createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  createTitle: (title) => {
+    return 'The ' + title;
+  },
+  buildMainCharacter: (name, age, pronouns) => {
+    return {
+      name:name,
+      age:age,
+      pronouns:pronouns
+    }
+  },
+  saveReview: (review, array) => {
+    // ensure no duplicates
+    if (!array.length) return array.push(review);
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === review) return
+    }
+    return array.push(review);
+  },
+  calculatePageCount: (title) => {
+    return title.length * 20;
+  },
+  writeBook: (bookTitle, bookCharacter, genre) => {
+    return {
+      title: bookTitle,
+      mainCharacter: bookCharacter,
+      pageCount: bookTitle.length * 20,
+      genre: genre,
+    }
+  },
+  editBook: (book) => {
+    book.pageCount *= .75
+  }
 }
