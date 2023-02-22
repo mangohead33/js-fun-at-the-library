68c1abd6b335a9b76e220fe34091b5f59ce8bde6
Calvin Chau <cachau33@gmail.com>
2023-02-21 17:25:17 -0800

    Updated test suite and passed test

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,24 @@
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook: (book, shelf) => {
+    if (shelf.length < 3) shelf.unshift(book);
+  },
+  unshelfBook: (book, shelf) => {
+    for (let i = 0; i < shelf.length; i++) {
+      if (shelf[i].title === book) shelf.splice(i, 1);
+    }
+  },
+  listTitles: (arr) => {
+    let result = '';
+    for (let movie of arr) {
+      result += (movie.title + ', ')
+    }
+    return result.slice(0, result.length - 2);
+  },
+  searchShelf: (arrShelf, title) => {
+    for (let i = 0; i < arrShelf.length; i++) {
+      if (arrShelf[i].title === title) return true
+    }
+    return false;
+  }
 };
