02348209819a39ef0d6f5176bef9d9e15807bb62
Calvin Chau <cachau33@gmail.com>
2023-02-21 17:44:43 -0800

    Updated and passed test

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,28 @@
 
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary: (name) => {
+    return {
+      name: name,
+      shelves: {
+        fantasy:[],
+        fiction:[],
+        nonFiction:[],
+      }
+    }
+  },
+  addBook: (library, book, genre) => {
+    library.shelves[book.genre].push(book);
+  },
+  checkoutBook: (library, book, genre) => {
+    let shelf = library.shelves[genre];
+    for (let i = 0; i < shelf.length; i++) {
+      if (shelf[i].title === book) {
+        shelf.splice(i,1);
+        return `You have now checked out ${book} from the ${library.name}`;
+      }
+    }
+    return `Sorry, there are currently no copies of ${book} available at the ${library.name}`
+
+  }
 };
